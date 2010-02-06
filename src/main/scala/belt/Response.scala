bcaa2c223c5fc90de6baf2d5cc8876b12583c161
Nick Partridge <nkpart@gmail.com>
2010-02-06 15:10:41 +1000

    exposed response through reader, it will do until I work out what the response needs

--- src/main/scala/belt/Response.scala
+++ src/main/scala/belt/Response.scala
@@ -2,10 +2,10 @@ package belt
 
 import scalaz.http._
 import scalaz.http.response._
-import scalaz.http.response.{Response => _}
 
 trait Response {
   val status: Status
   val headers: List[(ResponseHeader, String)]
-  val body: Stream[Byte]
+
+  def apply(response: SResponse): SResponse
 }
--- src/main/scala/belt/package.scala
+++ src/main/scala/belt/package.scala
@@ -5,10 +5,11 @@ import scalaz.http.response.{Response => SzResponse}
 package object belt {
   type SRequest = scalaz.http.request.Request[Stream]
   type SResponse = scalaz.http.response.Response[Stream] 
-  
-  def strap(f: Request => Response): SRequest => SResponse = request => {
+
+  def strap(f: Request => Response): SRequest => SResponse = implicit request => {
     val response = f(new Request { val underlying = request })
     val headers = response.headers map (t2 => (t2._1, t2._2.charsNelErr("empty header wtf")))
-    SzResponse.statusResponse(response.status, headers, response.body)(request)
+    val emptySR = SzResponse.emptyStatusResponse[Stream,Stream](response.status, headers)
+    response(emptySR)
   }
 }
